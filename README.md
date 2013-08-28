85c0b676c1e9bb7bc18287e0503bd3e7038abebe
Claudio Bredfeldt <claudio@cbmono.de>
2013-08-28 12:40:18 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 js-array-to-querystring
 =======================
 
-Transforms a JS key:value array into a valid query string: ?categoryId[]=4&amp;color[]=red&amp;color[]=blue&amp;price=150
+Transforms a JS key:value array (or object) into a valid url query string
+Useful in combination with $.url() (https://github.com/allmarkedup/purl), basically, the opposite of $.url().param()
+
+Uses:
+-----
+underscore.js (https://github.com/jashkenas/underscore)
+
+Usage:
+------
+$.url().param().toQueryString()
---
+++ array-to-querystring.js
@@ -0,0 +1,32 @@
+Object.prototype.toQueryString = function() {
+	
+	var response = '';
+	var pairs = _.pairs( this );
+
+	_.each( pairs, function( pair ) {
+		if ((_.isArray( pair[1] ) && pair[1].length) || pair[1] !== '') {
+			var field = pair[0];
+					field += _.isArray( pair[1] ) ? '[]' : '';
+					field += '=';
+
+			if (_.isArray( pair[1] )) {
+				var tmpField = '';
+				_.each( pair[1], function( value ) {
+					tmpField += field + value + '&';
+				});
+
+				response += tmpField;
+			}
+			else if (_.isBoolean( pair[1] )) {
+				if (pair[1]) {
+					response += field + '1' + '&';
+				}
+			}
+			else {
+				response += field + pair[1] + '&';
+			}
+		}
+	});
+
+	return response.substr(0, response.length - 1);
+};
